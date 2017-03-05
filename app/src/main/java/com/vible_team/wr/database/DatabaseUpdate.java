710d77bdba2b46ffb6ae8570f00cae79997c7d87
Taras Lishchynskiy <4enutl@gmail.com>
2017-03-05 22:45:21 +0200

    DBUpdate init

---
+++ app/src/main/java/com/vible_team/wr/database/DatabaseUpdate.java
@@ -0,0 +1,11 @@
+package com.vible_team.wr.database;
+
+import android.database.sqlite.SQLiteDatabase;
+
+class DatabaseUpdate {
+    SQLiteDatabase sqLiteDatabase;
+
+    DatabaseUpdate(SQLiteDatabase sqLiteDatabase) {
+        this.sqLiteDatabase = sqLiteDatabase;
+    }
+}
