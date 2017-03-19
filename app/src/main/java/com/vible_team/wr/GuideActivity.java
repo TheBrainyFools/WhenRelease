0e08af379fb323b518e70738ee1ede75b18e55ce
Taras Lishchynskiy <4enutl@gmail.com>
2017-03-19 17:23:48 +0200

    GuideActivity for app guide

---
+++ app/src/main/java/com/vible_team/wr/GuideActivity.java
@@ -0,0 +1,31 @@
+package com.vible_team.wr;
+
+import android.os.Bundle;
+
+import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
+import com.codemybrainsout.onboarder.AhoyOnboarderCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuideActivity extends AhoyOnboarderActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setColorBackground(R.color.colorPrimary);
+        setFinishButtonTitle("Get them! :)");
+
+        AhoyOnboarderCard zero = new AhoyOnboarderCard("Hi!", "Welcome to WhenRelease!");
+
+        List<AhoyOnboarderCard> pages = new ArrayList<>();
+        pages.add(zero);
+
+        setOnboardPages(pages);
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+
+    }
+}
