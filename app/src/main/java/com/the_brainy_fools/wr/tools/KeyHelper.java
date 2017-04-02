461a43b5f6e87d86574495400ede9923d20a201d
Taras Lishchynskiy <4enutl@gmail.com>
2017-04-02 14:20:59 +0300

    KeyHelper fix

--- app/src/main/java/com/the_brainy_fools/wr/tools/KeyHelper.java
+++ app/src/main/java/com/the_brainy_fools/wr/tools/KeyHelper.java
@@ -3,4 +3,11 @@ package com.the_brainy_fools.wr.tools;
 public class KeyHelper {
     public static final String API_BASE = "http://api.themoviedb.org/3/";
     public static final String API_KEY = "api_key=fe497b618e596d47a41279dafb0d1cbf";
+
+    public static final String MOVIE_SOON_URL_REQUEST = API_BASE + "discover/movie/?sort_by=popularity.desc&" + API_KEY;
+    public static final String MOVIE_ID_URL_REQUEST = API_BASE + "movie/";
+
+    public static final String MOVIE_CINEMA_URL_REQUEST = API_BASE + "movie/now_playing?" + API_KEY;
+
+    public static final String SERIAL_AIRING_URL_REQUEST = API_BASE + "tv/airing_today?" + API_KEY;
 }
