8400b367effe8cd9b68492c855212134baa0546d
Taras Lishchynskiy <4enutl@gmail.com>
2017-05-21 14:43:37 +0300

    Add: guide activity + drawables

--- app/src/main/java/com/the_brainy_fools/wr/GuideActivity.java
+++ app/src/main/java/com/the_brainy_fools/wr/GuideActivity.java
@@ -1,5 +1,6 @@
 package com.the_brainy_fools.wr;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
@@ -16,16 +17,28 @@ public class GuideActivity extends AhoyOnboarderActivity {
         setColorBackground(R.color.colorPrimary);
         setFinishButtonTitle("Get them! :)");
 
-        AhoyOnboarderCard zero = new AhoyOnboarderCard("Hi!", "Welcome to WhenRelease!");
+        AhoyOnboarderCard zero = new AhoyOnboarderCard("Hi!", "Welcome to the WhenRelease!!", R.drawable.ic_splash);
+        AhoyOnboarderCard first = new AhoyOnboarderCard("Follow it!", "Follow interesting movies!", R.drawable.guide_follow);
+        AhoyOnboarderCard second = new AhoyOnboarderCard("Remind!", "You will get notification when new movie comes out!", R.drawable.guide_reminder);
+        AhoyOnboarderCard third = new AhoyOnboarderCard("Watched!", "Mark watched movies, and we will hide them!", R.drawable.guide_watched);
+        AhoyOnboarderCard fourth = new AhoyOnboarderCard("Favorite!", "Mark your favorite movies, and we will show you similar!", R.drawable.guide_favourite);
+        AhoyOnboarderCard fifth = new AhoyOnboarderCard("Rate it!", "Rate the film and the program - get more!", R.drawable.guide_rate);
+        AhoyOnboarderCard sixth = new AhoyOnboarderCard("So...", "Other features you will see in the tooltips!\nEnjoy it!", R.drawable.guide_finish);
 
         List<AhoyOnboarderCard> pages = new ArrayList<>();
         pages.add(zero);
+        pages.add(first);
+        pages.add(second);
+        pages.add(third);
+        pages.add(fourth);
+        pages.add(fifth);
+        pages.add(sixth);
 
         setOnboardPages(pages);
     }
 
     @Override
     public void onFinishButtonPressed() {
-
+        startActivity(new Intent(this, MainActivity.class));
     }
 }
